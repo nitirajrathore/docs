86aeacf04931874042b7bc28835046d33df78879
Nitiraj Singh Rathore <nitiraj.rathore@gmail.com>
2017-04-10 13:13:57 +0530

    adding mind map of ambari's features

---
+++ ambari_presentation/ambari_mind_map.mm
@@ -0,0 +1,93 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1491061115907" ID="ID_1919963809" MODIFIED="1491062044690" TEXT="Apache Ambari Presentation">
+<node CREATED="1491061136461" ID="ID_1116214847" MODIFIED="1491061141994" POSITION="right" TEXT="topics to cover">
+<node CREATED="1491061256027" ID="ID_1770160749" MODIFIED="1491061267271" TEXT="Ambari Installation">
+<node CREATED="1491061329206" ID="ID_145293977" MODIFIED="1491809188481" TEXT="Smart Configs">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1491061338676" ID="ID_1329549121" MODIFIED="1491809192012" TEXT="cluster recommendations">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1491061376868" ID="ID_655691008" MODIFIED="1491061432762" TEXT="passwords of services"/>
+</node>
+<node CREATED="1491061463763" ID="ID_1862659773" MODIFIED="1491809105984" TEXT="Monitoring">
+<icon BUILTIN="flag"/>
+<node CREATED="1491061471713" ID="ID_1843402447" MODIFIED="1491809098178" TEXT="Ambari metrices">
+<icon BUILTIN="flag"/>
+</node>
+<node CREATED="1491809072815" ID="ID_912840751" MODIFIED="1491809094009" TEXT="Ambari Alerts">
+<icon BUILTIN="flag"/>
+</node>
+<node CREATED="1491061477713" ID="ID_405986505" MODIFIED="1491809100747" TEXT="SmartSense">
+<icon BUILTIN="flag"/>
+</node>
+<node CREATED="1491062111902" ID="ID_1627415025" MODIFIED="1491809103419" TEXT="CLUSTER ALERT CHECKS">
+<icon BUILTIN="flag"/>
+</node>
+</node>
+<node CREATED="1491062085507" ID="ID_1384826655" MODIFIED="1491809223253" TEXT="Log Search">
+<icon BUILTIN="flag"/>
+</node>
+<node CREATED="1491061492318" ID="ID_941625648" MODIFIED="1491061985627" TEXT="Extensible">
+<node CREATED="1491061510569" ID="ID_911723574" MODIFIED="1491809238551" TEXT="Ambari Stacks">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Links :
+    </p>
+    <ul>
+      <li>
+        https://hortonworks.com/apache/ambari/#section_3
+      </li>
+      <li>
+        https://hortonworks.com/hadoop-tutorial/introducing-apache-ambari-deploying-managing-apache-hadoop/
+      </li>
+    </ul>
+  </body>
+</html></richcontent>
+<icon BUILTIN="button_ok"/>
+<attribute_layout NAME_WIDTH="33" VALUE_WIDTH="33"/>
+</node>
+</node>
+<node CREATED="1491061387225" ID="ID_1616895260" MODIFIED="1491061392027" TEXT="Security Details">
+<node CREATED="1491061392858" ID="ID_1457349835" MODIFIED="1491809812472" TEXT="Ranger">
+<icon BUILTIN="flag"/>
+</node>
+<node CREATED="1491061396075" ID="ID_1493456720" MODIFIED="1491809815908" TEXT="Kerberos">
+<icon BUILTIN="flag"/>
+</node>
+<node CREATED="1491061401708" ID="ID_1839262959" MODIFIED="1491809818981" TEXT="LDAP">
+<icon BUILTIN="flag"/>
+</node>
+</node>
+<node CREATED="1491061268152" ID="ID_1154146429" MODIFIED="1491809828984" TEXT="Cluster Installation">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1491061274732" ID="ID_1437921199" MODIFIED="1491809831599" TEXT="Blueprints">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1491061294373" ID="ID_441175357" MODIFIED="1491809837574" TEXT="Ambari Agent + Ambari Server Info">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1491061307118" ID="ID_882988837" MODIFIED="1491809846786" TEXT="Views Server">
+<icon BUILTIN="button_cancel"/>
+</node>
+<node CREATED="1491061322717" ID="ID_1678077773" MODIFIED="1491809849683" TEXT="Ambari Views">
+<icon BUILTIN="button_ok"/>
+</node>
+</node>
+<node CREATED="1491061143242" ID="ID_1310466810" MODIFIED="1491064513446" POSITION="right" TEXT="Q and A ">
+<node CREATED="1491061159772" ID="ID_70354357" MODIFIED="1491061169490" TEXT="Mention HCC "/>
+<node CREATED="1491064516149" ID="ID_1774908365" MODIFIED="1491064525012" TEXT="My Questions :">
+<node CREATED="1491064526106" ID="ID_1632507303" MODIFIED="1491809899153" TEXT="Can we delete a node after installing services on it">
+<icon BUILTIN="help"/>
+</node>
+</node>
+<node CREATED="1491061170704" ID="ID_1208103845" MODIFIED="1491061247348" TEXT="mention Hortonworks  tutorrials"/>
+</node>
+</node>
+</map>
